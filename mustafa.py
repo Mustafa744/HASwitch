c5a3d7b0674f7978c74309292bf7bafccab75cd8
mustafa ashraf <mustafaashraf744@gmail.com>
2023-09-17 02:34:25 +0300

    dixing ip issue in configuration

--- mustafa.py
+++ mustafa.py
@@ -12,79 +12,12 @@ async def discover():
     LOGGER.debug("Discovered devices: %s", [{"address": device.ip, "name": device.name} for device in devices])
     return [device for device in devices if device.name.startswith("switch")]
 
-# class GodoxInstance:
-#     def __init__(self, ip, unique_id: str) -> None:
-#         self._ip = ip
-#         self._device = BleakClient(self._mac)
-#         self._is_on = None
-#         self._connected = None
-#         self._brightness = None
-#         self._unique_id = unique_id
-
-#     async def _send(self, data: bytearray):
-#         LOGGER.debug(''.join(format(x, ' 03x') for x in data))
-        
-#         if (not self._connected):
-#             await self.connect()
-        
-#         crcinst = Crc8Maxim()
-#         crcinst.process(data)
-#         await self._device.write_gatt_char(WRITE_UUID, data + crcinst.finalbytes())
-
-#     @property
-#     def mac(self):
-#         return self._mac
-
-#     @property
-#     def unique_id(self):
-#         return self._unique_id
-#     @property
-#     def is_on(self):
-#         return self._is_on
-
-#     @property
-#     def brightness(self):
-#         return self._brightness
-
-#     async def set_brightness(self, intensity: int):
-#         header = bytes.fromhex("f0d10501")
-#         command = bytes([intensity])
-#         params = bytes.fromhex("380c01")
-
-#         await self._send(header + command + params)
-
-#         self._brightness = intensity
-
-#     async def turn_on(self):
-#         header = bytes.fromhex("f0d0060c")
-#         command = bytes.fromhex("01")
-#         params = bytes.fromhex("00000000")
-
-#         await self._send(header + command + params)
-#         self._is_on = True
-
-#     async def turn_off(self):
-#         header = bytes.fromhex("f0d0060c")
-#         command = bytes.fromhex("00")
-#         params = bytes.fromhex("00000000")
-
-#         await self._send(header + command + params)
-#         self._is_on = False
-
-#     async def connect(self):
-#         await self._device.connect(timeout=20)
-#         await asyncio.sleep(1)
-#         self._connected = True
-
-#     async def disconnect(self):
-#         if self._device.is_connected:
-#             await self._device.disconnect()            
             
 class MustafaInstance:
-    def __init__(self,ip:str,port:str,unique_id:str) -> None:
-        self._ip = ip
+    def __init__(self,host:str,port:str,unique_id:str) -> None:
+        self._host = host
         self._port = port
-        self._device = IpSwitch(self.ip,self.port)
+        self._device = IpSwitch(self.host,self.port)
         self._is_on = None
         self._connected = None
         self._unique_id = unique_id
@@ -97,8 +30,8 @@ class MustafaInstance:
         pass
     
     @property
-    def ip(self):
-        return self._ip
+    def host(self):
+        return self._host
     
     @property
     def port(self):
