7699b6e5b7efd295c4e23d4bfc935b2e5dc7cae4
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-17 07:52:18 +0300

    ..

--- ip_switch.py
+++ ip_switch.py
@@ -1,59 +1,57 @@
-import requests
-from homeassistant.core import HomeAssistant
+import asyncio
+import aiohttp
+
 class IpSwitch:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self._is_connected = False
-        self.hass = HomeAssistant
         
     async def connect(self):
-        res = await requests.get('http://'+self.ip+':'+self.port+'/connect')
-        if res.status_code == 200:
-            self._is_connected = True
-            return True
-        else:
-            self._is_connected = False
-            return False
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://'+self.ip+':'+self.port+'/connect') as response:
+                if response.status == 200:
+                    self._is_connected = True
+                    return True
+                else:
+                    self._is_connected = False
+                    return False
         
-    
-    def disconnect(self):
-        res = requests.get('http://'+self.ip+':'+self.port+'/disconnect')
-        if res.status_code == 200:
-            self._is_connected = False
-            return True
-        else:
-            self._is_connected = True
-            return False
+    async def disconnect(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://'+self.ip+':'+self.port+'/disconnect') as response:
+                if response.status == 200:
+                    self._is_connected = False
+                    return True
+                else:
+                    self._is_connected = True
+                    return False
         
     # create property is connected
     def is_connected(self):
         return self._is_connected 
     
     async def send_data(self, data):
-        res = await requests.get('http://'+self.ip+':'+self.port+f'/{data}')
-        if res.status_code == 200:
-            return True
-        else:
-            return False
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://'+self.ip+':'+self.port+f'/{data}') as response:
+                if response.status == 200:
+                    return True
+                else:
+                    return False
     
-    def is_on(self):
-        res = requests.get('http://'+self.ip+':'+self.port+'/is_on')
-        if res.status_code == 200:
-            return res.json()['is_on']
-        else:
-            return False
-    # def turn_on(self):
-    #     return self.send_data('on')
-    
-    # def turn_off(self):
-    #     return self.send_data('off')
+    async def is_on(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://'+self.ip+':'+self.port+'/is_on') as response:
+                if response.status == 200:
+                    return (await response.json())['is_on']
+                else:
+                    return False
     
     @staticmethod
-    def discover_devices(ip:str,port:str)->dict:
-        res = requests.get('http://'+ip+':'+port+'/get_devices')
-        if res.status_code == 200:
-            # return devices as dict with keys ip , port , name 
-            return res.json()
-        else:
-            return {}
+    async def discover_devices(ip:str,port:str)->dict:
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://'+ip+':'+port+'/get_devices') as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return {}
